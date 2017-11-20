a40aa134255f8a8cfdb0e6f10c788c618cc111f9
Colin Greczkowski <cgreczkowski@mogiletech.com>
2017-11-20 15:03:47 -0500

    Preliminary "structure" of each page. No styling yet.
    Some basic url parameters are passed/received by several pages. I've added a few comments on the html pages explaining where DB data will go and how it will roughly be formatted
    
    All HREFS are absolute, with directory "site" being the root (/). Be sure to point your WAMP document root to "site".
    
    Finally, some code is commented out to prevent redirection by non-auth. users. This is for testing, and should be uncommented before release!

---
+++ site/js/functions.js
@@ -0,0 +1,46 @@
+// Global/widely used functions
+// Note; this file is included on all pages, so use document.ready or similar with caution!
+
+
+function getUrlParameter(name){	
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++)
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == name)
+        {
+            return sParameterName[1];
+        }
+    }
+	return false;
+}
+
+// Cookies
+function createCookie(name, value, days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        var expires = "; expires=" + date.toGMTString();
+    }
+    else var expires = "";               
+
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+
+//Redirect a non-logged in user to the login page
+function redirectOut(){
+	alert("You must be logged in to access this site.");
+	window.location.href = "/index.html";
+}
