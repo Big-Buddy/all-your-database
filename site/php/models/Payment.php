ccd37a0dae5f1dbb76491a9b6462bbb4aaa3fc91
Adriel Fabella <adriel.fab@gmail.com>
2017-11-27 22:29:54 -0500

    [Update] Adding the Payment model

---
+++ site/php/models/Payment.php
@@ -0,0 +1,16 @@
+<?php
+	
+	class Payment {
+		public $paymentID;
+		public $payingUserID;
+		public $rentedSpaceID;
+		public $amountInCADCents;
+		public $cardNumber;
+		public $cardExpiryDate;
+		public $cardSecurityCode;
+		public $cardholderName;
+		public $cardCompany;
+		public $cardType;
+		public $paymentDate;
+	}
+?>
