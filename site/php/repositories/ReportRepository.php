58ef22ad4ef45c3919242ca198542d66e067e783
Adriel Fabella <adriel.fab@gmail.com>
2017-12-01 22:48:59 -0500

    [Update] Adding report8 SQL query to backend

--- site/php/repositories/ReportRepository.php
+++ site/php/repositories/ReportRepository.php
@@ -91,6 +91,21 @@
             return $this->returnResult($sql);
         }
 
+        public function report8() {
+            $sql = "Select Stores.StoreID, Stores.StoreName, Ads.Category from Stores
+                        inner join Users on Users.UserID = Stores.ManagerUserID
+                    inner join RentedSpaces on RentedSpaces.StoreID = Stores.StoreID
+                    inner join Ads on Ads.AdID = RentedSpaces.AdID
+                    where Users.AddressProvince like '%%'
+                    group by Stores.StoreID, Stores.StoreName, Ads.Category";
+            return $this->returnResult($sql);
+        }
+
+        public function report9() {
+            $sql = "SELECT * FROM Ads WHERE Category IN ('RentElectronics', 'Car', 'Apartment', 'WeddingDresses')";
+            return $this->returnResult($sql);
+        }
+
         public function returnResult($sql) {
             $result = $this->connection->query($sql);
             closeConnection($this->connection);
