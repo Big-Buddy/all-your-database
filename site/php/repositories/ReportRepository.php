f3d691e39d5b2815377a010f1129348e590b656b
Adriel Fabella <adriel.fab@gmail.com>
2017-12-01 22:38:36 -0500

    [Update] Adding report5 SQL query to backend

--- site/php/repositories/ReportRepository.php
+++ site/php/repositories/ReportRepository.php
@@ -47,7 +47,25 @@
             $result = $this->connection->query($sql);
             closeConnection($this->connection);
             return $result;
+        }
 
+        public function report5() {
+            $sql = "Select numRatingsCat.PostingUserID, maxRatingsCat.Category, maxRatingsCat.maxRating,  Users.*
+                        from (
+                            Select max(Rating) maxRating, Category from (
+                                select PostingUserID, adID, Rating, Category from Ads
+                                    inner join Ratings on AdIDBeingRated = adID
+                            ) numRatingsCat
+                            group by Category
+                        ) maxRatingsCat
+                        inner join 
+                            (select PostingUserID, adID, Rating, Category from Ads inner join Ratings on AdIDBeingRated = adID) numRatingsCat
+                        on numRatingsCat.Category = maxRatingsCat.Category and numRatingsCat.Rating = maxRatingsCat.maxRating
+                        inner join Users on Users.UserID = numRatingsCat.PostingUserID
+                        where AddressCity like '%%'";
+            $result = $this->connection->query($sql);
+            closeConnection($this->connection);
+            return $result;
         }
 
     }
---
+++ site/php/scripts/report5.php
@@ -0,0 +1,37 @@
+<?php
+require_once '../repositories/ReportRepository.php';
+
+$reportRepository = new ReportRepository();
+$result = $reportRepository->report5();
+$isSuccess = false;
+
+$array = [];
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $object = new stdClass();
+        $object->postingUserID = $row["PostingUserID"];
+        $object->category = $row["Category"];
+        $object->maxRating = $row["maxRating"];
+        $object->userID = $row["UserID"];
+        $object->password = $row["Password"];
+        $object->userType = $row["UserType"];
+        $object->membershipPlan = $row["MembershipPlan"];
+        $object->phone = $row["Phone"];
+        $object->email = $row["Email"];
+        $object->addressStreet = $row["AddressStreet"];
+        $object->addressCity = $row["AddressCity"];
+        $object->addressProvince = $row["AddressProvince"];
+        $object->addressCountry = $row["AddressCountry"];
+        $object->addressPostalCode = $row["AddressPostalCode"];
+        array_push($array, $object);
+    }
+    $isSuccess = true;
+}
+
+if ($isSuccess) {
+    echo json_encode($array);
+} else {
+    header('HTTP/1.1 500 Server Error');
+    die(json_encode(array('message' => 'No value was returned')));
+}
+?>
