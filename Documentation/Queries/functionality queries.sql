e934f84756a891dc79a1cdbf9d64bc265c6920c0
Big-Buddy <a.pelletier0x7c9@gmail.com>
2017-12-03 13:53:36 -0500

    delete ad query implemented and tested

--- Documentation/Queries/functionality queries.sql
+++ Documentation/Queries/functionality queries.sql
@@ -17,3 +17,23 @@ ORDER BY PaymentDate ASC;
 /* Insert new rating for an ID */
 INSERT INTO Ratings (AdIDBeingRated, UserIDRatingAd, Rating)
 VALUES (AD_IN_CONTEXT, USER_THAT_IS_RATING, STARS_GIVEN);
+
+/* Delete an ad and all rows that reference it */
+DELETE FROM Ratings
+WHERE AdIDBeingRated = AD_BEING_DELETED;
+
+UPDATE Payments
+SET Payments.RentedSpaceID = NULL
+WHERE PayingUserID = 
+	(SELECT Ads.PostingUserID 
+		FROM RentedSpaces INNER JOIN Ads ON RentedSpaces.AdID = Ads.AdID
+		WHERE RentedSpaces.AdID = AD_BEING_DELETED);
+
+DELETE FROM RentedSpaces
+WHERE AdID = AD_BEING_DELETED;
+
+DELETE FROM Images
+WHERE AdID = AD_BEING_DELETED;
+
+DELETE FROM Ads
+WHERE AdID = AD_BEING_DELETED;
